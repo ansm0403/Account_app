3bfc3cf4158a712da86d30062b71d08a53c4b332
an-sangmoon <tkdans312@gmail.com>
2025-01-16 05:08:47 +0900

    CI next-auth fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,32 +1,6 @@
-import { User } from '@/model/user';
-import NextAuth, { NextAuthOptions } from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
 
-
-export const authOptions : NextAuthOptions = {
-    providers: [
-        // OAuth authentication providers...
-        GoogleProvider({
-          clientId: process.env.GOOGLE_ID as string,
-          clientSecret: process.env.GOOGLE_SECRET as string
-        }),
-      ],
-      callbacks : {
-        session({session, token}){
-            if(session.user){
-                (session.user as User).id = token.sub as string;
-            }
-            return session;
-        }
-      },
-      session : {
-        strategy : 'jwt',
-      },
-      pages : {
-        signIn : '/auth/signin',
-      }
-
-}
+import { authOptions } from '@/app/auth/authOptions';
+import NextAuth from 'next-auth'
 
 export const  handler = NextAuth(authOptions);
 
